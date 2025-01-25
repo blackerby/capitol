5260d9756d20e911411b8adbdd168066cf25beab
William Blackerby <wmblackerby@gmail.com>
2025-01-25 18:57:31 -0500

    happy path tokenizing done

--- src/lib.rs
+++ src/lib.rs
@@ -22,53 +22,50 @@ pub(crate) const BASE_URL: &str = "https://www.congress.gov";
 #[derive(Debug, PartialEq)]
 struct Congress(Vec<u8>);
 
-//impl Display for Congress<'_> {
-//    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//        write!(f, "{}", self.0)
-//    }
-//}
-
-//impl Congress {
-//    fn as_ordinal(&self) -> String {
-//        if self.0.ends_with('1') {
-//            format!("{self}st")
-//        } else if self.0.ends_with('2') {
-//            format!("{self}nd")
-//        } else if self.0.ends_with('3') {
-//            format!("{self}rd")
-//        } else {
-//            format!("{self}th")
-//        }
-//    }
-//
-//    fn to_number(&self) -> usize {
-//        self.0.parse::<usize>().unwrap()
-//    }
-//}
-
 #[derive(Debug, PartialEq)]
 enum Chamber {
     House,
     Senate,
 }
 
-// TODO: tokenize first
-fn tokenize(input: &str) -> (Vec<u8>, Vec<u8>, Vec<u8>) {
+fn tokenize(input: &str) -> (Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>) {
     let mut iter = input.as_bytes().iter().peekable();
+
+    // initialize containers for various parts of the citation
     let mut congress_bytes: Vec<u8> = Vec::with_capacity(3);
     let mut chamber_byte: Vec<u8> = Vec::with_capacity(1);
-    //let mut type_bytes: Vec<u8> = Vec::with_capacity(7);
+    let mut type_bytes: Vec<u8> = Vec::with_capacity(7);
+    let mut number_bytes: Vec<u8> = Vec::new();
+    let mut ver_bytes: Vec<u8> = Vec::new();
+
     while let Some(&ch) = iter.next_if(|&&ch| ch > b'0' && ch <= b'9') {
         congress_bytes.push(ch);
     }
-    // TODO: peek instead?
+
     while let Some(&ch) = iter.next_if(|&&ch| ch == b'h' || ch == b'H' || ch == b's' || ch == b'S')
     {
         chamber_byte.push(ch);
     }
 
-    let remainder: Vec<u8> = iter.map(|b| *b).collect();
-    (congress_bytes, chamber_byte, remainder)
+    while let Some(&ch) = iter.next_if(|&&ch| ch.is_ascii_alphabetic()) {
+        type_bytes.push(ch);
+    }
+
+    while let Some(&ch) = iter.next_if(|&&ch| ch.is_ascii_digit()) {
+        number_bytes.push(ch);
+    }
+
+    while let Some(&ch) = iter.next_if(|&&ch| ch.is_ascii_alphabetic()) {
+        ver_bytes.push(ch);
+    }
+
+    (
+        congress_bytes,
+        chamber_byte,
+        type_bytes,
+        number_bytes,
+        ver_bytes,
+    )
 }
 
 #[cfg(test)]
@@ -76,9 +73,29 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_parse_hand_rolled() {
+    fn test_tokenize_no_ver() {
         let mut input = "118hr8070";
-        let expected = (b"118".to_vec(), b"h".to_vec(), b"r8070".to_vec());
+        let expected = (
+            b"118".to_vec(),
+            b"h".to_vec(),
+            b"r".to_vec(),
+            b"8070".to_vec(),
+            Vec::new(),
+        );
+        let result = tokenize(&mut input);
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_tokenize_with_ver() {
+        let mut input = "118hr8070ih";
+        let expected = (
+            b"118".to_vec(),
+            b"h".to_vec(),
+            b"r".to_vec(),
+            b"8070".to_vec(),
+            b"ih".to_vec(),
+        );
         let result = tokenize(&mut input);
         assert_eq!(expected, result);
     }
@@ -88,23 +105,4 @@ mod test {
         assert_eq!(*CURRENT_YEAR, 2025);
         assert_eq!(*CURRENT_CONGRESS, 119);
     }
-
-    //#[test]
-    //fn test_congress_as_ordinal() {
-    //    let congress = Congress("111");
-    //    let ordinal = congress.as_ordinal();
-    //    assert_eq!("111st", ordinal);
-    //
-    //    let congress = Congress("112");
-    //    let ordinal = congress.as_ordinal();
-    //    assert_eq!("112nd", ordinal);
-    //
-    //    let congress = Congress("113");
-    //    let ordinal = congress.as_ordinal();
-    //    assert_eq!("113rd", ordinal);
-    //
-    //    let congress = Congress("116");
-    //    let ordinal = congress.as_ordinal();
-    //    assert_eq!("116th", ordinal);
-    //}
 }
