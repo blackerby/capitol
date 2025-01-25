ed91661db1a005d7b8f6fe63b1d68755c3315b69
William Blackerby <wmblackerby@gmail.com>
2025-01-25 16:21:57 -0500

    remove chrono dependency

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,24 @@
 
 mod legislation;
 
-use chrono::Datelike;
 //use std::fmt::Display;
 use std::sync::LazyLock;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-pub(crate) const FIRST_CONGRESS: usize = 1789;
+pub(crate) const FIRST_CONGRESS: u64 = 1789;
 // only dealing with common era years
 // Right to use UTC?
 #[allow(clippy::cast_sign_loss)]
-static CURRENT_YEAR: LazyLock<usize> = LazyLock::new(|| chrono::Utc::now().year() as usize);
-pub(crate) static CURRENT_CONGRESS: LazyLock<usize> =
-    LazyLock::new(|| *CURRENT_YEAR - FIRST_CONGRESS / 2 + 1);
+static CURRENT_YEAR: LazyLock<u64> = LazyLock::new(|| {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+        / 31536000 // seconds in year
+        + 1970 // UNIX_EPOCH year
+});
+pub(crate) static CURRENT_CONGRESS: LazyLock<u64> =
+    LazyLock::new(|| (*CURRENT_YEAR - FIRST_CONGRESS) / 2 + 1);
 pub(crate) const BASE_URL: &str = "https://www.congress.gov";
 
 #[derive(Debug, PartialEq)]
@@ -48,25 +55,23 @@ enum Chamber {
     Senate,
 }
 
-fn parse(input: &str) -> (Congress, Chamber, Vec<u8>) {
+// TODO: tokenize first
+fn tokenize(input: &str) -> (Vec<u8>, Vec<u8>, Vec<u8>) {
     let mut iter = input.as_bytes().iter().peekable();
     let mut congress_bytes: Vec<u8> = Vec::with_capacity(3);
-    let mut chamber = Chamber::House;
+    let mut chamber_byte: Vec<u8> = Vec::with_capacity(1);
     //let mut type_bytes: Vec<u8> = Vec::with_capacity(7);
     while let Some(&ch) = iter.next_if(|&&ch| ch > b'0' && ch <= b'9') {
         congress_bytes.push(ch);
     }
+    // TODO: peek instead?
     while let Some(&ch) = iter.next_if(|&&ch| ch == b'h' || ch == b'H' || ch == b's' || ch == b'S')
     {
-        if ch == b'h' || ch == b'H' {
-            chamber = Chamber::House;
-        } else {
-            chamber = Chamber::Senate;
-        }
+        chamber_byte.push(ch);
     }
 
     let remainder: Vec<u8> = iter.map(|b| *b).collect();
-    (Congress(congress_bytes), chamber, remainder)
+    (congress_bytes, chamber_byte, remainder)
 }
 
 #[cfg(test)]
@@ -76,9 +81,15 @@ mod test {
     #[test]
     fn test_parse_hand_rolled() {
         let mut input = "118hr8070";
-        let expected = (Congress(b"118".to_vec()), Chamber::House, b"r8070".to_vec());
-        let result = parse(&mut input);
+        let expected = (b"118".to_vec(), b"h".to_vec(), b"r8070".to_vec());
+        let result = tokenize(&mut input);
         assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_year_and_congress() {
+        assert_eq!(*CURRENT_YEAR, 2025);
+        assert_eq!(*CURRENT_CONGRESS, 119);
     }
 
     //#[test]
