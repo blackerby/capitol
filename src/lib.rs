7dabef97718e3dcc462d2fe396542c305a39994c
William Blackerby <wmblackerby@gmail.com>
2025-01-26 10:27:44 -0500

    separate out constants and statics

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,9 @@
 // TODO: add tests for each legislation type
 // TODO: add CLI
 // TODO: test sad path
+mod constants;
 
-use std::sync::LazyLock;
-use std::time::{SystemTime, UNIX_EPOCH};
-
-const FIRST_CONGRESS: u64 = 1789;
-static CURRENT_YEAR: LazyLock<u64> = LazyLock::new(|| {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap() // TODO: change to expect?
-        .as_secs()
-        / 31536000 // seconds in year
-        + 1970 // UNIX_EPOCH year
-});
-static CURRENT_CONGRESS: LazyLock<u64> = LazyLock::new(|| (*CURRENT_YEAR - FIRST_CONGRESS) / 2 + 1);
-//const BASE_URL: &str = "https://www.congress.gov";
-
-const BILL_VERSIONS: [&[u8]; 37] = [
-    b"as", b"ash", b"ath", b"ats", b"cdh", b"cds", b"cph", b"cps", b"eah", b"eas", b"eh", b"enr",
-    b"es", b"fph", b"fps", b"hds", b"ih", b"iph", b"ips", b"is", b"lth", b"lts", b"pap", b"pcs",
-    b"pp", b"rch", b"rcs", b"rds", b"rfh", b"rfs", b"rh", b"rhuc", b"rih", b"rs", b"rth", b"rts",
-    b"sc",
-];
+use crate::constants::{BILL_VERSIONS, CURRENT_CONGRESS};
 
 #[derive(Debug, PartialEq)]
 struct Version(String);
