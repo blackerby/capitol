d0294025d53da37aa5d2b4f42d0920e77fb04cb3
William Blackerby <wmblackerby@gmail.com>
2025-01-26 17:31:20 -0500

    more error work

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-use std::{num::ParseIntError, string::FromUtf8Error};
+use std::{fmt::Display, num::ParseIntError, string::FromUtf8Error};
 
 #[derive(Debug)]
 pub enum Error {
@@ -8,6 +8,25 @@ pub enum Error {
     MissingBillVersion,
     InvalidCongress,
     UnknownCongObjectType,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::FromUtf8(e) => Display::fmt(e, f),
+            Self::ParseInt(e) => Display::fmt(e, f),
+            Self::InvalidBillVersion => f.write_str("not a valid bill version"),
+            Self::MissingBillVersion => {
+                f.write_str("url with bill version requested but no version given")
+            }
+            Self::InvalidCongress => {
+                f.write_str("congress number in citation has not occurred yet")
+            }
+            Self::UnknownCongObjectType => {
+                f.write_str("unknown or unsupported congressional object type")
+            }
+        }
+    }
 }
 
 impl From<FromUtf8Error> for Error {
@@ -21,3 +40,5 @@ impl From<ParseIntError> for Error {
         Self::ParseInt(value)
     }
 }
+
+impl std::error::Error for Error {}
