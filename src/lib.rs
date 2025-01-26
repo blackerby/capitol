ae8b207987b4aeb654722c4a63d7285b05987961
William Blackerby <wmblackerby@gmail.com>
2025-01-25 19:29:25 -0500

    add CiteBytes struct

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,15 @@ const BILL_VERSIONS: [&str; 38] = [
     "rfh", "rfs", "rh", "rhuc", "rih", "rs", "rth", "rts", "sc", "",
 ];
 
+#[derive(Debug, Default, PartialEq)]
+struct CiteBytes {
+    congress: Vec<u8>,
+    chamber: u8,
+    object_type: Vec<u8>,
+    number: Vec<u8>,
+    ver: Option<Vec<u8>>,
+}
+
 #[derive(Debug, PartialEq)]
 struct Congress(Vec<u8>);
 
@@ -31,44 +40,51 @@ enum Chamber {
     Senate,
 }
 
-fn tokenize(input: &str) -> (Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>) {
+fn tokenize(input: &str) -> CiteBytes {
     let mut iter = input.as_bytes().iter().peekable();
 
     // initialize containers for various parts of the citation
     let mut congress_bytes: Vec<u8> = Vec::with_capacity(3);
-    let mut chamber_byte: Vec<u8> = Vec::with_capacity(1);
     let mut type_bytes: Vec<u8> = Vec::with_capacity(7);
     let mut number_bytes: Vec<u8> = Vec::new();
     let mut ver_bytes: Vec<u8> = Vec::new();
+
+    // initialize parts container
+    let mut parts = CiteBytes::default();
 
     while let Some(&ch) = iter.next_if(|&&ch| ch > b'0' && ch <= b'9') {
         congress_bytes.push(ch);
     }
 
-    while let Some(&ch) = iter.next_if(|&&ch| ch == b'h' || ch == b'H' || ch == b's' || ch == b'S')
-    {
-        chamber_byte.push(ch);
+    parts.congress = congress_bytes.clone();
+
+    if let Some(&ch) = iter.next_if(|&&ch| ch == b'h' || ch == b'H' || ch == b's' || ch == b'S') {
+        parts.chamber = ch;
     }
 
     while let Some(&ch) = iter.next_if(|&&ch| ch.is_ascii_alphabetic()) {
         type_bytes.push(ch);
     }
 
+    parts.object_type = type_bytes;
+
     while let Some(&ch) = iter.next_if(|&&ch| ch.is_ascii_digit()) {
         number_bytes.push(ch);
     }
+
+    parts.number = number_bytes;
 
     while let Some(&ch) = iter.next_if(|&&ch| ch.is_ascii_alphabetic()) {
         ver_bytes.push(ch);
     }
 
-    (
-        congress_bytes,
-        chamber_byte,
-        type_bytes,
-        number_bytes,
-        ver_bytes,
-    )
+    if ver_bytes.is_empty() {
+        parts.ver = None;
+    } else {
+        parts.ver = Some(ver_bytes);
+    }
+
+    parts
 }
 
 #[cfg(test)]
@@ -78,13 +94,13 @@ mod test {
     #[test]
     fn test_tokenize_no_ver() {
         let mut input = "118hr8070";
-        let expected = (
-            b"118".to_vec(),
-            b"h".to_vec(),
-            b"r".to_vec(),
-            b"8070".to_vec(),
-            Vec::new(),
-        );
+        let expected = CiteBytes {
+            congress: b"118".to_vec(),
+            chamber: b'h',
+            object_type: b"r".to_vec(),
+            number: b"8070".to_vec(),
+            ver: None,
+        };
         let result = tokenize(&mut input);
         assert_eq!(expected, result);
     }
@@ -92,13 +108,13 @@ mod test {
     #[test]
     fn test_tokenize_with_ver() {
         let mut input = "118hr8070ih";
-        let expected = (
-            b"118".to_vec(),
-            b"h".to_vec(),
-            b"r".to_vec(),
-            b"8070".to_vec(),
-            b"ih".to_vec(),
-        );
+        let expected = CiteBytes {
+            congress: b"118".to_vec(),
+            chamber: b'h',
+            object_type: b"r".to_vec(),
+            number: b"8070".to_vec(),
+            ver: Some(b"ih".to_vec()),
+        };
         let result = tokenize(&mut input);
         assert_eq!(expected, result);
     }
