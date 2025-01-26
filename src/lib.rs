f86a8df316395c2168bbc75aea1dbe1cb9af8d99
William Blackerby <wmblackerby@gmail.com>
2025-01-25 22:46:09 -0500

    add some types, add some tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+// TODO: add tests for each legislation type
 
 use std::sync::LazyLock;
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -16,11 +17,14 @@ pub(crate) static CURRENT_CONGRESS: LazyLock<u64> =
     LazyLock::new(|| (*CURRENT_YEAR - FIRST_CONGRESS) / 2 + 1);
 pub(crate) const BASE_URL: &str = "https://www.congress.gov";
 
-const BILL_VERSIONS: [&str; 38] = [
+const BILL_VERSIONS: [&str; 37] = [
     "as", "ash", "ath", "ats", "cdh", "cds", "cph", "cps", "eah", "eas", "eh", "enr", "es", "fph",
     "fps", "hds", "ih", "iph", "ips", "is", "lth", "lts", "pap", "pcs", "pp", "rch", "rcs", "rds",
-    "rfh", "rfs", "rh", "rhuc", "rih", "rs", "rth", "rts", "sc", "",
+    "rfh", "rfs", "rh", "rhuc", "rih", "rs", "rth", "rts", "sc",
 ];
+
+#[derive(Debug, PartialEq)]
+struct Version<'s>(&'s str);
 
 #[derive(Debug, Default, PartialEq)]
 struct CiteBytes {
@@ -32,7 +36,23 @@ struct CiteBytes {
 }
 
 #[derive(Debug, PartialEq)]
-struct Congress(Vec<u8>);
+struct Congress(u64);
+
+impl Congress {
+    fn parse(input: Vec<u8>) -> Self {
+        match String::from_utf8(input) {
+            Ok(s) => {
+                let congress = s
+                    .parse::<u64>()
+                    .expect("could not convert input to integer");
+                Congress(congress)
+            }
+            _ => {
+                todo!()
+            }
+        }
+    }
+}
 
 #[derive(Debug, PartialEq)]
 enum Chamber {
@@ -40,9 +60,46 @@ enum Chamber {
     Senate,
 }
 
-struct Capitol;
+impl Chamber {
+    fn parse(input: u8) -> Self {
+        if input.to_ascii_lowercase() == b'h' {
+            Self::House
+        } else {
+            Self::Senate
+        }
+    }
+}
 
-impl Capitol {
+#[derive(Debug, PartialEq)]
+enum CongObjectType {
+    S,
+    Hr,
+    SRes,
+    HRes,
+    SConRes,
+    HConRes,
+    SJRes,
+    HJRes,
+    // TODO: add committee report types
+}
+
+//impl CongObjectType {
+//    fn parse(input: Vec<u8>, chamber: Chamber) -> Self {
+//        match input {
+//        }
+//    }
+//}
+
+#[derive(Debug, PartialEq)]
+struct Citation<'s> {
+    congress: Congress,
+    chamber: Chamber,
+    object_type: CongObjectType,
+    number: usize,
+    ver: Option<Version<'s>>,
+}
+
+impl Citation<'_> {
     pub fn tokenize(input: &str) -> CiteBytes {
         let mut iter = input.as_bytes().iter().peekable();
 
@@ -90,6 +147,11 @@ impl Capitol {
 
         parts
     }
+
+    //pub fn parse(input: CiteBytes) -> Self {
+    //    let congress = Congress::parse(input.congress);
+    //    let chamber = Chamber::parse(input.chamber);
+    //}
 }
 
 #[cfg(test)]
@@ -97,7 +159,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_tokenize_no_ver() {
+    fn test_tokenize_no_ver_house_bill() {
         let mut input = "118hr8070";
         let expected = CiteBytes {
             congress: b"118".to_vec(),
@@ -106,21 +168,49 @@ mod test {
             number: b"8070".to_vec(),
             ver: None,
         };
-        let result = Capitol::tokenize(&mut input);
+        let result = Citation::tokenize(&mut input);
         assert_eq!(expected, result);
     }
 
     #[test]
-    fn test_tokenize_with_ver() {
-        let mut input = "118hr8070ih";
+    fn test_tokenize_no_ver_senate_bill() {
+        let mut input = "118s5";
+        let expected = CiteBytes {
+            congress: b"118".to_vec(),
+            chamber: b's',
+            object_type: Vec::new(),
+            number: b"5".to_vec(),
+            ver: None,
+        };
+        let result = Citation::tokenize(&mut input);
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_tokenize_with_ver_house_bill() {
+        let mut input = "118hr555ih";
         let expected = CiteBytes {
             congress: b"118".to_vec(),
             chamber: b'h',
             object_type: b"r".to_vec(),
-            number: b"8070".to_vec(),
+            number: b"555".to_vec(),
             ver: Some(b"ih".to_vec()),
         };
-        let result = Capitol::tokenize(&mut input);
+        let result = Citation::tokenize(&mut input);
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_tokenize_with_ver_senate_bill() {
+        let mut input = "118s17is";
+        let expected = CiteBytes {
+            congress: b"118".to_vec(),
+            chamber: b's',
+            object_type: Vec::new(),
+            number: b"17".to_vec(),
+            ver: Some(b"is".to_vec()),
+        };
+        let result = Citation::tokenize(&mut input);
         assert_eq!(expected, result);
     }
 }
